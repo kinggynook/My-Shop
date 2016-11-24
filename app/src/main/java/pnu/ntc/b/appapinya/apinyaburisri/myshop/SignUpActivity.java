316273e4ec59cb6f9a175931a69235cbfaadc088
ApinyaKINGGY <kinggy_hoo@hotmail.com>
2016-11-24 16:09:46 +0700

    form Sign Up

---
+++ app/src/main/java/pnu/ntc/b/appapinya/apinyaburisri/myshop/SignUpActivity.java
@@ -0,0 +1,13 @@
+package pnu.ntc.b.appapinya.apinyaburisri.myshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
